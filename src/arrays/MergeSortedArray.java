e81103e0930c57617b5ba912f7eed2a09a2404a8
vspace <evanilsondev@gmail.com>
2021-06-05 14:32:54 -0500

    designing strategies to understand problems

--- src/arrays/MergeSortedArray.java
+++ src/arrays/MergeSortedArray.java
@@ -4,13 +4,25 @@ public class MergeSortedArray {
 
 	public static void main(String[] args) {
 
-		/*int[] nums1 = new int[] {-1,0,0,3,3,3,0,0,0};
-		int[] nums2 = new int[] {1, 2, 2};
-		int m = 6, n = 3;*/
+		/*int[] nums1 = new int[] {1, 2, 3, 0, 0, 0};
+		int[] nums2 = new int[] {2, 5, 6};
+		int m = 3, n = 3;*/
 		
-		int[] nums1 = new int[] {0};
+		/*int[] nums1 = new int[] {0};
 		int[] nums2 = new int[] {1};
-		int m = 0, n = 1;
+		int m = 0, n = 1;*/
+		
+		/*int[] nums1 = new int[] {-1,3,0,0,0,0,0};
+		int[] nums2 = new int[] {0,0,1,2,3};
+		int m = 2, n = 5;*/
+		
+		/*int[] nums1 = new int[] {1, 2, 3, 0, 0};
+		int[] nums2 = new int[] {4, 5};
+		int m = 3, n = 2;*/
+		
+		int[] nums1 = new int[] {0, 0, 0, 0};
+		int[] nums2 = new int[] {1, 2, 3};
+		int m = 0, n = 3;
 		
 		merge(nums1, m, nums2, n);
 		
@@ -21,22 +33,13 @@ public class MergeSortedArray {
 	public static void merge(int[] nums1, int m, int[] nums2, int n) {
 		int j = 0, i = 0;
 		
-        while(i < m + n) {
-        	if(j < n) {
-        		if(nums2[j] <= nums1[i]) {
-            		shiftToRight(nums1, i, nums2[j]);
-            		j++;
-            	}
-        		
-        		if(i + 1 > n) {
-        			if(nums1[i] == 0) {
-            			shiftToRight(nums1, i, nums2[j]);
-                		j++;
-            		}
-        		}
+        while(i < nums2.length && j < nums1.length) {
+        	if(nums2[i] <= nums1[j] || (j >= m && nums1[j] == 0)) {
+        		shiftToRight(nums1, j, nums2[i]);
+        		i++;
         	}
         	
-        	i++;
+        	j++;
         }
     }
 	
